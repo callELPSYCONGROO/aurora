52b77c15741ead09d085841c373484abed93fba6
無痕剑 <mayh0991@126.com>
2017-12-01 16:40:18 +0800

    通过测试

--- business/src/test/java/com/wuhenjian/aurora/business/BusinessApplicationTests.java
+++ business/src/test/java/com/wuhenjian/aurora/business/BusinessApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wuhenjian.aurora.config;
+package com.wuhenjian.aurora.business;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ConfigApplicationTests {
+public class BusinessApplicationTests {
 
 	@Test
 	public void contextLoads() {
