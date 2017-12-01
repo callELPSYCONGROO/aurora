bfd3af8ea7bff308d38a0e6b535eb20dc41f478b
無痕剑 <mayh0991@126.com>
2017-12-01 18:03:33 +0800

    测试分支

--- business/src/main/java/com/wuhenjian/aurora/business/asd/TestCtrl.java
+++ business/src/main/java/com/wuhenjian/aurora/business/asd/TestCtrl.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 测试分支
  * @author 無痕剑
  * @date 2017/11/30 14:00
  */
