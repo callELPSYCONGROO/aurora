92629f9083e15a3c5a2ca6745eb318bab7e20f26
callELPSYCONGROO <mayh0991@126.com>
2017-12-05 23:05:41 +0800

    接口测试

--- member-service/src/main/java/com/wuhenjian/aurora/memberservice/controller/api/MemberLoginController.java
+++ member-service/src/main/java/com/wuhenjian/aurora/memberservice/controller/api/MemberLoginController.java
@@ -1,9 +1,13 @@
 package com.wuhenjian.aurora.memberservice.controller.api;
 
 import com.wuhenjian.aurora.memberservice.controller.BaseController;
+import com.wuhenjian.aurora.utils.entity.result.ApiResult;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author SwordNoTrace
@@ -18,5 +22,13 @@ public class MemberLoginController extends BaseController{
     @RequestMapping("yige")
     public String gege() {
         return yige;
+    }
+
+    @RequestMapping("login")
+    public ApiResult login(String name, String pwd) {
+        Map<String,Object> map = new HashMap<>();
+        map.put("name", name);
+        map.put("pwd", pwd);
+        return ApiResult.success(map);
     }
 }
