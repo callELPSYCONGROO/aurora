68959fcf932eff1cb02f8d9fe781da7f015e71e2
無痕剑 <mayh0991@126.com>
2018-01-11 14:02:59 +0800

    接口规则重置，每一个项目的接口段为下面所在数字的10位，即在本地可以部署10台： # configserver-7701,monitor-7801,ra-7901,db-9611     member:                   #9621     game:                     #9631     notify:                   #9641     rabbitmq:                 #9651     file:                     #9661     album:                    #9671     auth:                     #9681     php:                      #9691

---
+++ php-service/src/main/java/com/wuhenjian/aurora/phpservice/PhpServiceApplication.java
@@ -0,0 +1,12 @@
+package com.wuhenjian.aurora.phpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhpServiceApplication.class, args);
+	}
+}
