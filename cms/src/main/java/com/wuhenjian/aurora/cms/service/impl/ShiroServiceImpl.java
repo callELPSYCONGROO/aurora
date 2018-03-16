ada1bcd84776a39771e372ecf431d20a3da36660
無痕剑 <mayh0991@126.com>
2018-03-16 18:05:31 +0800

    CMS页面

--- cms/src/main/java/com/wuhenjian/aurora/cms/service/impl/ShiroServiceImpl.java
+++ cms/src/main/java/com/wuhenjian/aurora/cms/service/impl/ShiroServiceImpl.java
@@ -12,7 +12,6 @@ import org.apache.shiro.web.servlet.AbstractShiroFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
